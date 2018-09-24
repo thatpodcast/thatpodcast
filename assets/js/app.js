d7491d334e12a030e4d4ac5f9a1a8481cecdf4a8
Beau Simensen <beau@dflydev.com>
2018-09-24 16:41:49 -0500

    Enable SASS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,4 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
