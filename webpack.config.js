fae1d1d060147c62fcb99ace92651e869198bb71
Beau Simensen <beau@dflydev.com>
2018-09-24 16:38:16 -0500

    Add Encore

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
