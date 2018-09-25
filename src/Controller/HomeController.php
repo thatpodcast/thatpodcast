cc1b93da996356989298b30e0e4541841599f2ed
Beau Simensen <beau@dflydev.com>
2018-09-25 16:09:28 -0500

    Add about and newsletter; moved index to home.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home.html.twig', [
             'controller_name' => 'Yay',
         ]);
     }
