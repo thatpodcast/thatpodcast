09d778c0fc62d914837940309c279c8f4811733e
Beau Simensen <beau@dflydev.com>
2018-11-25 01:52:41 -0600

    Final touches on a few things

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: vendor/bin/heroku-php-nginx -C heroku/nginx.conf public
-messenger: bin/console messenger:consume-messages amqp --limit=100 --time-limit=3600
+messenger: bin/console messenger:consume-messages --limit=100 --time-limit=3600
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 require('../css/app.scss');
 
-require('../images/red-mic.jpg');
+require('../images/about.jpg');
+require('../images/beau.jpg');
+require('../images/dave.jpg');
 require('../images/hexellence.png');
+require('../images/newsletter.jpg');
+require('../images/red-mic.jpg');
+require('../images/that-podcast-cover-photo.jpg');
 require('../svg/thatpodcast-logo.svg');
